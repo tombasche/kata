33d49579054a6878510edd2b28ed31486637cea8
Tom Basche <tcbasche@gmail.com>
2020-12-26 16:17:46 +1100

    Calculate wealth within a 2D array

--- wealth/wealth.go
+++ wealth/wealth.go
@@ -1,5 +1,21 @@
 package wealth
 
-func Wealthiest(input [][]int) int {
-	return 6
+func Wealthiest(input [][]int) (sum int) {
+
+	for _, arr := range input {
+		t := sumSlice(arr)
+		if t > sum {
+			sum = t
+		}
+	}
+
+	return
+}
+
+func sumSlice(input []int) int {
+	var total int
+	for _, i := range input {
+		total += i
+	}
+	return total
 }
