a1100b5be211056032a2ab67c95acc56324ebbae
Tom Basche <tcbasche@gmail.com>
2020-12-26 16:31:40 +1100

    Solve distribution of candies question

--- candy/candy.go
+++ candy/candy.go
@@ -1,5 +1,23 @@
 package candy
 
 func DistributeCandies(input []int, extraCandies int) []bool {
-	return []bool{true, true, true, false, true}
+
+	var result []bool
+
+	most := max(input)
+	for _, c := range input {
+		total := c + extraCandies
+		result = append(result, total >= most)
+	}
+	return result
+}
+
+func max(input []int) int {
+	m := 0
+	for _, val := range input {
+		if val > m {
+			m = val
+		}
+	}
+	return m
 }
